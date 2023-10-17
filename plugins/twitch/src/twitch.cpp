0cc57df5b35f00476f7aab199690a7df0eca3b8b
jmautari <mautari@gmail.com>
2023-10-17 00:15:15 -0700

    Add copyright notice

--- plugins/twitch/src/twitch.cpp
+++ plugins/twitch/src/twitch.cpp
@@ -1,3 +1,28 @@
+/**
+ * Widget Sensors
+ * Twitch plug-in
+ * Copyright (C) 2021-2023 John Mautari - All rights reserved
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "twitch.hpp"
 #include "shared/logger.hpp"
 #include "shared/string_util.h"
@@ -334,8 +359,9 @@ void TwitchClient::SetBroadcastInfo(const std::string& game_id,
     auto res = cli.Patch(fmt::format("{}?broadcaster_id={}",
                              Twitch::Endpoint::kChannels, user_id),
         headers, s.str(), kApplicationUrlEncode);
-    if (!res)
-      LOG(ERROR) << "Can't set broadcast info";
+    if (!res || res->status != 204)
+      LOG(ERROR) << "Can't set broadcast info. Status: "
+                 << (res ? res->status : -1);
     else
       LOG(INFO) << "Broadcast info set successfully";
   } catch (...) {
@@ -374,7 +400,7 @@ nlohmann::json TwitchGame::GetGameInfo(std::string const& game_name,
   httplib::Params params;
   params.emplace("name", game_name);
   auto res = cli.Get(Twitch::Endpoint::kGames, params, headers);
-  if (!res) {
+  if (!res || res->status != 200) {
     LOG(ERROR) << "Could not find data for " << game_name;
     return {};
   }
