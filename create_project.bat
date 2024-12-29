a25b178ce17f734031d56da620ef7574658ca7ac
jmautari <mautari@gmail.com>
2024-12-28 17:44:13 -0800

    Fix project creation

--- create_project.bat
+++ create_project.bat
@@ -3,17 +3,17 @@ setlocal
 
 PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer;%PATH%
 
-for /f "usebackq delims=" %%i in (`vswhere.exe -latest -version "[16.4,16.79]" -requires Microsoft.Component.MSBuild -property installationPath`) do (
+for /f "usebackq delims=" %%i in (`vswhere.exe -latest -version "[17.0,17.99]" -requires Microsoft.Component.MSBuild -property installationPath`) do (
   set InstallDir=%%i
   if not exist "%%i\MSBuild\Current\Bin\MSBuild.exe" (
-    echo error: Cannot locate Visual Studio 2019.
+    echo error: Cannot locate Visual Studio 2022.
     goto error_out
   )
 )
 
 set __VCVARS=%InstallDir%\VC\Auxiliary\Build
 
-call "%__VCVARS%\vcvarsall.bat" amd64 10.0.19041.0 -vcvars_ver=14.2 %*
+call "%__VCVARS%\vcvarsall.bat" amd64 10.0.26100.0 -vcvars_ver=14.3 %*
 
 :build
 mkdir build
